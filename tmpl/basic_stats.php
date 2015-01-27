f31fed0f9301b39449f032f59957d7a7775d1be8
joaoromao <joaoromao@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-27 13:28:22 +0000

    release 1.1.3 version
    git-svn-id: https://plugins.svn.wordpress.org/wp-share-buttons-analytics-by-getsocial/tags/1.1.3@1076654 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tmpl/basic_stats.php
@@ -0,0 +1,24 @@
+<h2>
+    <i class="fa fa-bar-chart"></i>Stats
+    <? $from = date('Y-m-d', strtotime('-8 days')) ?>
+    <? $to = date('Y-m-d', strtotime('-1 days')) ?>
+    <small>from <strong><? echo $from ?></strong> to <strong><? echo $to ?></strong></small>
+    <a href="http://getsocial.io/dashboard" target="_blank"><i class="fa fa-area-chart"></i>Detailed Stats</a>
+</h2>
+
+<div class="graphs gs-clearfix" data-graph-api="<?php echo $GS->api_url('sites/analytics/'.get_option('gs-api-key')) ?>">
+    <div class="graph">
+        <h4><i class="fa fa-group"></i>Visits</h4>
+        <h3 class="total_visits">...</h5>
+    </div>
+    <div class="graph">
+        <h4><i class="fa fa-share-alt"></i>Shares</h4>
+        <h3 class="total_shares">...</h5>
+    </div>
+    <div class="graph">
+        <h4><i class="fa fa-sign-in"></i>Leads</h4>
+        <h3 class="total_leads">...</h5>
+    </div>
+</div>
+
+<div class="cl"></div>
